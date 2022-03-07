9889e5dc31040f608f3eff57bae064f3befdc8dd
schmitzs1 <aclassyfox44@gmail.com>
2022-03-07 15:24:00 -0600

    actual movement

--- SunStaff/Scripts/Milo/MiloMovement.gd
+++ SunStaff/Scripts/Milo/MiloMovement.gd
@@ -1,24 +1,18 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export (int) var speed = 200
 
-var newVelocity = Vector2()
 onready var sprite = self.get_node("Sprite")
 var faceRight = true
-<<<<<<< Updated upstream
-=======
 export (int) var speed = 300
 export (int) var jump_speed = -1100
 export (int) var gravity = 3000
 export (float, 0, 1.0) var friction = 0.2
 export (float, 0, 1.0) var acceleration = 0.25
->>>>>>> Stashed changes
 
-func _ready():
-	self.mode = MODE_CHARACTER
+var velocity = Vector2.ZERO
 
 func get_input():
-	var velocity = Vector2()
+	var dir = 0
 	if Input.is_action_just_pressed("Right"):
 		if (!faceRight):
 			sprite.scale.x *= -1
@@ -27,21 +21,17 @@ func get_input():
 		if (faceRight):
 			sprite.scale.x *= -1
 			faceRight = false
-	if Input.is_action_just_pressed("Jump"):
-		velocity.y -= 1
-
 	if Input.is_action_pressed("Right"):
-		velocity.x += 1
+		dir += 1
 	if Input.is_action_pressed("Left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("Jump"):
-		velocity.y -= 1
-	velocity.y += 0.0
-	newVelocity = velocity.normalized() * speed
-
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+	
 func _physics_process(delta):
 	get_input()
-	self.linear_velocity = newVelocity
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_just_pressed("Jump"):
@@ -49,4 +39,3 @@ func _physics_process(delta):
 			velocity.y = jump_speed
 		
 	
-
