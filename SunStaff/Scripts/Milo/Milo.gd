ed8f611cb9d98fe0edcd78a5b0291133fed251df
schmitzs1 <aclassyfox44@gmail.com>
2022-03-23 15:06:28 -0500

    sprint+

--- SunStaff/Scripts/Milo/Milo.gd
+++ SunStaff/Scripts/Milo/Milo.gd
@@ -35,10 +35,11 @@ var faceRight = true
 export (int) var speed = 300
 export (int) var jump_speed = -1100
 export (int) var gravity = 3000
-export (float, 0, 1.0) var friction = 0.2
+export (float, 0, 1.0) var friction = 0.5
 export (float, 0, 1.0) var acceleration = 0.25
 var velocity = Vector2.ZERO
 var justJumped = false
+
 
 func _ready():
 	playerRootNode = get_parent()
@@ -68,10 +69,7 @@ func get_input():
 	if (GameManager.IsPlayerAlive and GameManager.IsGamePlaying):
 		# Movement
 		var dir = 0
-		if Input.is_action_pressed("sprint"):
-			sprintcheck = true
-		else:
-			sprintcheck = false
+		
 		if Input.is_action_just_pressed("Jump"): 
 			if is_on_floor():
 				velocity.y = jump_speed
@@ -114,6 +112,7 @@ func get_input():
 				dir += 2
 			else:
 				dir += 1
+				
 			if (justJumped):
 				if (HasStaff):
 					spriteUnlit.visible = false
@@ -138,16 +137,28 @@ func get_input():
 					sprite.animation = "MiloWalkStaffless"
 					spriteUnlit.animation = "MiloWalkStafflessUNLIT"
 				justJumped = false
-
+		
 		else:
 			if (HasStaff):
+				
+					spriteUnlit.visible = false
+					sprite.animation = "MiloIdleStaff"
+			else:
+				
+					spriteUnlit.visible = true
+					sprite.animation = "MiloIdleStaffless"
+					spriteUnlit.animation = "MiloIdleStafflessUNLIT"
+		if Input.is_action_pressed("sprint"):
+			sprintcheck = true
+			if (HasStaff):
 				spriteUnlit.visible = false
-				sprite.animation = "MiloIdleStaff"
+				sprite.animation = "MiloRunStaff"
 			else:
 				spriteUnlit.visible = true
-				sprite.animation = "MiloIdleStaffless"
-				spriteUnlit.animation = "MiloIdleStafflessUNLIT"
-		
+				sprite.animation = "MiloRunStaffless"
+				spriteUnlit.animation = "MiloRunStafflessUNLIT"
+		else:
+			sprintcheck = false
 		if dir != 0:
 			velocity.x = lerp(velocity.x, dir * speed, acceleration)
 		else:
@@ -208,6 +219,8 @@ func _physics_process(delta):
 		
 func PlayerDeath(position):
 	self.position = position
+	velocity.x = 0
+	velocity.y = 0
 	WithinAltarRange = false
 	WithinLeverRange = false
 	WithinPedestalRange = false
