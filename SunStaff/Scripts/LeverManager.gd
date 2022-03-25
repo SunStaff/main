28c386eba45af423f6120e28895c55b52a691120
Logan <pribyll2420@my.uwstout.edu>
2022-03-24 21:47:16 -0500

    Lever Manager Updated

--- SunStaff/Scripts/LeverManager.gd
+++ SunStaff/Scripts/LeverManager.gd
@@ -55,5 +55,50 @@ func Level3(lever, turnedOn):
 	match(name):
 		"1":
 			lever.visible = false
+		#Lever 1 of Door Puzzle [Lever Down y = 183, lever up y = -183]
 		"2":
+			var door1 = get_parent().get_node("Door1")
+			var door2 = get_parent().get_node("Door2")
+			var door4 = get_parent().get_node("Door4")
+			if(turnedOn):
+				door1.position.y = -183
+				door2.position.y = -183
+				door4.position.y = 183			
+			elif(!turnedOn):
+				door1.position.y = 183
+				door2.position.y = 183
+			pass
+		#Lever 2 of Door Puzzle
+		"3":
+			var door2 = get_parent().get_node("Door2")
+			var door4 = get_parent().get_node("Door4")
+			if(turnedOn):
+				door2.position.y = -183
+				door4.position.y = -183			
+			elif(!turnedOn):
+				door2.position.y = 183
+				door4.position.y = 183
+			pass
+		#Lever 3 of Door Puzzle
+		"4":
+			var door1 = get_parent().get_node("Door1")
+			var door3 = get_parent().get_node("Door3")
+			if(turnedOn):
+				door1.position.y = -183
+				door3.position.y = 183			
+			elif(!turnedOn):
+				door1.position.y = 183
+			pass
+		#Lever 4 of Door Puzzle
+		"5":
+			var door2 = get_parent().get_node("Door2")
+			var door3 = get_parent().get_node("Door3")
+			var door4 = get_parent().get_node("Door4")
+			if(turnedOn):
+				door2.position.y = 183
+				door3.position.y = -183
+				door4.position.y = -183			
+			elif(!turnedOn):
+				door3.position.y = 183
+				door4.position.y = 183
 			pass
