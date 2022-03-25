be2486341f1eb20943bccd9535bff91f14e8585f
hintzh0119 <hintzh0119@my.uwstout.edu>
2022-03-25 02:29:09 -0500

    Switches and Doors Added - No Interactions

--- SunStaff/Scripts/LeverManager.gd
+++ SunStaff/Scripts/LeverManager.gd
@@ -55,50 +55,51 @@ func Level3(lever, turnedOn):
 	match(name):
 		"1":
 			lever.visible = false
-		#Lever 1 of Door Puzzle [Lever Down y = 183, lever up y = -183]
+		#Lever 1 of Door Puzzle [Lever Down y = 252, lever up y = 110S]
+		#Doors for puzzle are offset by 1 -- Door2 is first puzzle door
 		"2":
-			var door1 = get_parent().get_node("Door1")
-			var door2 = get_parent().get_node("Door2")
-			var door4 = get_parent().get_node("Door4")
+			var door1 = get_parent().get_node("Door2")
+			var door2 = get_parent().get_node("Door3")
+			var door4 = get_parent().get_node("Door5")
 			if(turnedOn):
-				door1.position.y = -183
-				door2.position.y = -183
-				door4.position.y = 183			
+				door1.position.y = 110
+				door2.position.y = 110
+				door4.position.y = 252			
 			elif(!turnedOn):
-				door1.position.y = 183
-				door2.position.y = 183
+				door1.position.y = 252
+				door2.position.y = 252
 			pass
 		#Lever 2 of Door Puzzle
 		"3":
-			var door2 = get_parent().get_node("Door2")
-			var door4 = get_parent().get_node("Door4")
+			var door2 = get_parent().get_node("Door3")
+			var door4 = get_parent().get_node("Door5")
 			if(turnedOn):
-				door2.position.y = -183
-				door4.position.y = -183			
+				door2.position.y = 110
+				door4.position.y = 110			
 			elif(!turnedOn):
-				door2.position.y = 183
-				door4.position.y = 183
+				door2.position.y = 252
+				door4.position.y = 252
 			pass
 		#Lever 3 of Door Puzzle
 		"4":
-			var door1 = get_parent().get_node("Door1")
-			var door3 = get_parent().get_node("Door3")
+			var door1 = get_parent().get_node("Door2")
+			var door3 = get_parent().get_node("Door4")
 			if(turnedOn):
-				door1.position.y = -183
-				door3.position.y = 183			
+				door1.position.y = 110
+				door3.position.y = 252			
 			elif(!turnedOn):
-				door1.position.y = 183
+				door1.position.y = 110
 			pass
 		#Lever 4 of Door Puzzle
 		"5":
-			var door2 = get_parent().get_node("Door2")
-			var door3 = get_parent().get_node("Door3")
-			var door4 = get_parent().get_node("Door4")
+			var door2 = get_parent().get_node("Door3")
+			var door3 = get_parent().get_node("Door4")
+			var door4 = get_parent().get_node("Door5")
 			if(turnedOn):
-				door2.position.y = 183
-				door3.position.y = -183
-				door4.position.y = -183			
+				door2.position.y = 110
+				door3.position.y = 110
+				door4.position.y = 252			
 			elif(!turnedOn):
-				door3.position.y = 183
-				door4.position.y = 183
+				door3.position.y = 252
+				door4.position.y = 252
 			pass
