692036b297af7b8df19881465830e68f3f709269
Logan <pribyll2420@my.uwstout.edu>
2022-04-14 22:28:11 -0500

    Fixed Gem Puzzle and attempt to fix gem selection menu

--- SunStaff/Scripts/Objects/GemPedestal.gd
+++ SunStaff/Scripts/Objects/GemPedestal.gd
@@ -14,34 +14,41 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if (WithinPedestalRange):
+		var frameNumber = self.get_child(0).frame
 		if (Input.is_action_just_pressed("Interact")):
 			print()
 			print("interaction with pedestal")
 			print(GetCurrentClosestPedestal(GameManager.GetGemPedestals(), GameManager.GetPlayer()).name)
 			print(self.name)
 			if (GetCurrentClosestPedestal(GameManager.GetGemPedestals(), GameManager.GetPlayer()) == self):
-				if (self.get_child(0).frame != 0):
-					match self.get_child(0).frame:
+				if (frameNumber > 0):
+					print("Current Frame Number for " + self.name + ": " + str(frameNumber))
+					match frameNumber:
 						1:
 							GameManager.ToggleGem("Blue")
+							print("Blue Changed to No Gem")
 							self.get_child(0).frame = 0
-							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false)
+							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false, self)
 						2:
 							GameManager.ToggleGem("Green")
+							print("Green Changed to No Gem")
 							self.get_child(0).frame = 0
-							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false)
+							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false, self)
 						3:
 							GameManager.ToggleGem("Magenta")
+							print("Magenta Changed to No Gem")
 							self.get_child(0).frame = 0
-							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false)
+							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false, self)
 						4:
 							GameManager.ToggleGem("Cyan")
+							print("Cyan Changed to No Gem")
 							self.get_child(0).frame = 0
-							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false)
+							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false, self)
 						5:
 							GameManager.ToggleGem("Red")
+							print("Red Changed to No Gem")
 							self.get_child(0).frame = 0
-							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false)
+							GameManager.GetLevelManagers()[0].ChangePedestalBeamColors("", false, self)
 				else:
 					GameManager.GetLevelManagers()[0].OpenGemSelectionScreen(CurrentClosestPedestal)
 			
