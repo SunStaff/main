7a8a2be6793edbacc423de8941ce05f8fb6a7633
schmitzs1 <98604405+schmitzs1@users.noreply.github.com>
2022-04-19 19:57:41 -0500

    staff placement

--- SunStaff/Scripts/Levels/tutorial.gd
+++ SunStaff/Scripts/Levels/tutorial.gd
@@ -3,27 +3,31 @@ extends Node
 
 var tutorial_door1
 var tutorial_door2
-var StaffAltar
+var StaffAltartest
 var SunStaff
 var Altar_holding_staff
 var timer
 var StaffAltars = []
 var lever2
+var milo
 
 func _ready():
 	tutorial_door2 = get_parent().get_node("next_pedestal_to_end/door_endingdoor")
-	StaffAltar = get_parent().get_node("sun_staff_resting_place/StaffAltar resting place")
-	Altar_holding_staff =  get_parent().get_node("next_pedestal_to_end/staff_tutorial")
+	Altar_holding_staff = get_parent().get_node("sun_staff_resting_place/StaffAltar resting place")
+	StaffAltartest =  get_parent().get_node("next_pedestal_to_end/staff_tutorial")
 	lever2 = get_parent().get_node("next_pedestal_to_end/Lever2")
-	
+	milo = get_parent().get_node("Milo")
+	milo.HasStaff = false
+	milo.TurnLightOff = true
+	Altar_holding_staff.CurrentAltarWithStaff = Altar_holding_staff
 
 
 
 func _physics_process(delta):
 	
-	if Altar_holding_staff.activated:
+	if StaffAltartest.activated:
 		tutorial_door2.position.y = 780
-	elif !Altar_holding_staff.activated and !lever2.isTurnedOn:
+	elif !StaffAltartest.activated and !lever2.isTurnedOn:
 		tutorial_door2.position.y = 280
 
 
