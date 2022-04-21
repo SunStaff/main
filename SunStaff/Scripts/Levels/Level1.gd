280f595a7aa61216a5128092e8749bec7642decc
Logan <pribyll2420@my.uwstout.edu>
2022-04-21 11:36:27 -0500

    Update Level1.gd

--- SunStaff/Scripts/Levels/Level1.gd
+++ SunStaff/Scripts/Levels/Level1.gd
@@ -16,6 +16,26 @@ const POS1 = 2841
 const POS2 = 7425
 const POS3 = 12768
 
+#Get all puzzle platforms for a level 1 puzzle
+var Platforms = []
+var platform1
+var platform2
+var platform3
+
+#num * -INCREMENT determines puzzle platform's y-pos (tells current num for current position)
+#Current numbers are the defaults
+var num1 = 6
+var num2 = 0
+var num3 = 3
+#tells the newNum for its new position [Lerping causes inputs to be eaten depending on timing]
+# var newNum1 = 0
+# var newNum2 = 0
+# var newNum3 = 0
+#increment at which platforms move when num is increased by 1.
+const INCREMENT = 200
+#Lowest possible y-pos for puzzle platforms
+const PLATFORM_OFFSET = -269
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	movingAltar = get_parent().get_node("MovingAltar")
@@ -23,6 +43,13 @@ func _ready():
 	movingBlock = get_parent().get_node("MovingBlock")
 	drawBridge = get_parent().get_node("DrawBridge")
 
+	Platforms.clear()
+	#Gets Puzzle Platforms within the level
+	Platforms = get_tree().get_nodes_in_group("Platform")
+	platform1 = Platforms[0]
+	platform2 = Platforms[1]
+	platform3 = Platforms[2]
+	
 	timer = Timer.new()
 	self.add_child(timer)
 
@@ -82,3 +109,20 @@ func _on_EndLevel1_body_entered(body):
 func Level1End():
 	print("End of Level 1")
 	GameManager.ChangeScene()
+
+func FlippedPuzzleLever(leverNum):
+	if(leverNum == 1):
+		num1 = (num1 + 1) % 10
+		num3 = (num3 + 1) % 10
+		platform1.position.y = num1 * (-INCREMENT) + PLATFORM_OFFSET
+		platform3.position.y = num3 * (-INCREMENT) + PLATFORM_OFFSET
+	elif(leverNum == 2):
+		num1 = (num1 + 1) % 10
+		num2 = (num2 + 1) % 10
+		platform1.position.y = num1 * (-INCREMENT) + PLATFORM_OFFSET
+		platform2.position.y = num2 * (-INCREMENT) + PLATFORM_OFFSET
+	elif(leverNum == 3):
+		num2 = (num2 + 3) % 10
+		num3 = (num3 + 3) % 10
+		platform2.position.y = num2 * (-INCREMENT) + PLATFORM_OFFSET
+		platform3.position.y = num3 * (-INCREMENT) + PLATFORM_OFFSET
