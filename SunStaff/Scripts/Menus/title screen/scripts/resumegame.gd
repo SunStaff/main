c4cda753907fb64931cd409f5e60448f6eb88389
schmitzs1 <aclassyfox44@gmail.com>
2022-04-27 14:49:11 -0500

    pause menu changes

--- SunStaff/Scripts/Menus/title screen/scripts/resumegame.gd
+++ SunStaff/Scripts/Menus/title screen/scripts/resumegame.gd
@@ -1,8 +1,12 @@
 extends Control
 
+var pause
+var options
 func _unhandled_input(event):
-	if (event.is_action_pressed("pause") and not GameManager.GemSelectionScreenOpen):
+	if ((event.is_action_pressed("pause") and not GameManager.GemSelectionScreenOpen) and !options):
 		self.is_paused = !is_paused
+		get_node("options menu/VBoxContainer").visible = false
+		get_node("options menu/OPTIONS").visible = false
 
 var is_paused = false setget set_is_paused
 
@@ -18,3 +22,20 @@ func _on_resume_pressed():
 func _on_back_to_menu_pressed():
 	self.is_paused = false
 	GameManager.SceneTransition.transition_to("res://Scenes/Menus/MainMenu.tscn")
+
+
+func _on_options_pressed():
+	options = true
+	get_node("paused menu/VBoxContainer").visible = false
+	get_node("paused menu/paused").visible = false
+	get_node("options menu/VBoxContainer").visible = true
+	get_node("options menu/OPTIONS").visible = true
+	
+
+
+func _on_back_to_pause_pressed():
+	options = false
+	get_node("paused menu/VBoxContainer").visible = true
+	get_node("paused menu/paused").visible = true
+	get_node("options menu/VBoxContainer").visible = false
+	get_node("options menu/OPTIONS").visible = false
